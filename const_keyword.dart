ccd723cbb8f37e880350613e9b62061e8ad2924f
omar115 <omarhasan115@gmail.com>
2022-08-11 13:43:13 +0600

    study topics- const key, parse, if else combo

---
+++ const_keyword.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  const pi = 3.14;
+  const area = pi * 12 * 12;
+  print("The output is ${area}");
+}
---
+++ if_else_combined.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  var a = 10;
+  var res =
+      a > 12 ? "value is greater than 10" : "value is less than or equal to 10";
+  print(res);
+}
